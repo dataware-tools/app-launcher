4a7747dc5166e59d7363712a5f569cfa5ac43f5e
d-hayashi <hayashi.daiki@nagoya-u.jp>
2020-10-01 18:34:54 +0900

    Add Dockerfile and CI

---
+++ Dockerfile
@@ -0,0 +1,40 @@
+# syntax=docker/dockerfile:1.0.0-experimental
+FROM node:14.11.0-stretch as BUILD_STAGE
+SHELL ["/bin/bash", "-c"]
+ENV LANG="en_US.UTF-8"
+
+# Copy files
+RUN mkdir -p /opt/app
+COPY ./packag*.json /opt/app/
+WORKDIR /opt/app
+
+# Install dependency
+RUN mkdir -p -m 0600 ~/.ssh && ssh-keyscan github.com >> ~/.ssh/known_hosts
+RUN --mount=type=ssh yarn install --network-concurrency 1
+
+# Copy other files
+COPY ./src /opt/app/src
+COPY ./public /opt/app/public
+COPY ./craco.config.js /opt/app/
+COPY ./tsconfig.json /opt/app/
+
+# Build app
+RUN npm run build
+
+
+#
+# Second stage
+#
+FROM node:14.11.0-stretch-slim
+
+# Install server
+RUN npm install -g serve
+
+# Copy files
+RUN mkdir -p /opt/app
+COPY --from=BUILD_STAGE /opt/app/build /opt/app/build
+COPY ./public /opt/app/public
+WORKDIR /opt/app
+
+# Set default command
+CMD ["serve build -s -l 3000"]
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
 import React from "react";
 import "./App.css";
-
+import { Card, Icon } from "semantic-ui-react";
 import { PageWrapper } from "@dataware-tools/app-common";
 
 const App = () => {
   return (
     <PageWrapper>
-      <h1>Content</h1>
+      <Card.Group>
+        <Card href="/data-browser">
+          <Card.Content textAlign="center">
+            <Icon
+              name="database"
+              size="massive"
+              color="black"
+              style={{ marginBottom: "14px", marginRight: "0" }}
+            />
+            <Card.Header>Data Browser</Card.Header>
+            <Card.Description>Explore data</Card.Description>
+          </Card.Content>
+        </Card>
+        <Card href="/docs">
+          <Card.Content textAlign="center">
+            <Icon
+              name="book"
+              size="massive"
+              color="black"
+              style={{ marginBottom: "14px", marginRight: "0" }}
+            />
+            <Card.Header>Documents</Card.Header>
+            <Card.Description>Read documents</Card.Description>
+          </Card.Content>
+        </Card>
+      </Card.Group>
     </PageWrapper>
   );
 };
